eff562d88d29589d1f04e6f9fff5a9c4f11ac6ec
Niranjan Ravichandra <nravichandra@6river.com>
2019-12-01 13:45:48 -0500

    Final changes for now

--- lisp/config-languages.el
+++ lisp/config-languages.el
@@ -1,4 +1,4 @@
-;; Emacs config for languages
+; Emacs config for languages
 
 ;; Flycheck/Company/Lsp-mode
 (use-package flycheck
@@ -17,23 +17,34 @@
 (use-package lsp-mode
   :ensure t
   :commands lsp
+  :hook ((rust-mode python-mode) . lsp)
   :config (require 'lsp-clients))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+(use-package yasnippet
+  :ensure t)
 
 ;; Python
   (use-package elpy
   :ensure t
-  :init
-  (elpy-enable)
   :hook
-  (python-mode . lsp)
+  (python-mode . elpy-enable)
+
   :config
   (setq python-shell-interpreter "ipython"
         python-shell-interpreter-args "-i --simple-prompt"))
 
 ;; Rust
 (use-package rust-mode
-  :ensure t
-  :hook (rust-mode . lsp))
+  :ensure t)
 
 (use-package cargo
   :ensure t
@@ -42,7 +53,6 @@
 (use-package flycheck-rust
   :ensure t
   :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-(use-package toml-mode)
 
 ;; Matlab
 
