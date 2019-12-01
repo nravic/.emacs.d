bcdea5fd97db78dba9c3094d5d825cdad9fd9440
Niranjan Ravichandra <nravic@protonmail.ch>
2019-11-30 23:23:43 -0500

    Added everything

---
+++ lisp/config-languages.el
@@ -0,0 +1,58 @@
+;; Emacs config for languages
+
+;; Flycheck/Company/Lsp-mode
+(use-package flycheck
+  :ensure t
+  :disabled f
+  :init
+  (flycheck-mode 1))
+
+(use-package company
+  :ensure t
+  :hook (prog-mode . company-mode)
+  :config
+  (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1))
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :config (require 'lsp-clients))
+
+(use-package lsp-ui
+  :ensure t
+  )
+
+;; Python
+  (use-package elpy
+  :ensure t
+  :init
+  (elpy-enable)
+  :hook
+  (python-mode . lsp)
+  :config
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "-i --simple-prompt"))
+
+;; Rust
+(use-package rust-mode
+  :ensure t
+  :hook (rust-mode . lsp))
+
+(use-package cargo
+  :ensure t
+  :hook (rust-mode . cargo-minor-mode))
+
+(use-package flycheck-rust
+  :ensure t
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+;; Matlab
+
+
+;; Javascript
+
+
+;; sh
+
+(provide 'config-languages.el)
---
+++ lisp/config-misc.el
@@ -0,0 +1,73 @@
+;; Utils/UX stuff for emacs
+
+;; Ivy/counsel/swiper
+(use-package ivy
+  :ensure t
+  :init
+  (ivy-mode 1)
+
+  :config
+  (setq ivy-height 15)
+  (global-set-key "\C-s" 'swiper))
+
+(use-package counsel
+  :ensure t
+  :after ivy
+  :init
+  )
+
+;; config misc misc
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode 1))
+
+;; less useless help screen
+(use-package helpful
+ :ensure t
+ :commands (helpful-callable helpful-variable)
+ :init
+ (global-set-key (kbd "C-h k") #'helpful-key)
+ (global-set-key (kbd "C-h C") #'helpful-command)
+ (global-set-key (kbd "C-h .") #'helpful-at-point)
+ (global-set-key (kbd "C-h C-.") #'helpful-at-point))
+
+;; Magit
+(use-package magit
+  :defer t
+  ;; :commands magit-status
+  :ensure t
+  :bind ("C-x g" . magit-status)
+  :config
+  (define-key magit-mode-map (kbd "C-TAB") nil)
+  (define-key magit-mode-map (kbd "C-<tab>") nil)
+  ;; (dolist (keymap (list magit-diff-mode-map magit-log-mode-map))
+  ;;   (define-key keymap (kbd "C-TAB") nil)
+  ;;   (define-key keymap (kbd "C-<tab>") nil))
+  )
+
+;; Avy
+(use-package avy
+  :ensure t
+  :commands (avy-goto-word-1 avy-goto-char-2 avy-goto-char-timer)
+  :bind (("M-m" . 'avy-goto-word-1)
+         ("M-'" . 'avy-goto-char-2))
+  )
+
+;; Wrap-region
+(use-package wrap-region
+  :ensure t
+  :init (wrap-region-mode 1))
+
+;; UI stuff/themes/fonts
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(load-theme 'gruvbox-dark-hard t)
+
+;; rainbow mode
+
+
+
+(provide 'config-misc.el)
